4901ff076bb6deb9a45ec400e2bb52859ec2089b
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-17 11:12:50 -0500

    avance y modificaciones de prueba

--- Heap.java
+++ Heap.java
@@ -12,4 +12,34 @@ public class Heap<T extends Comparable<T>> {
         heap.add(item);
         percolateUp(heap.size() - 1);
     }
+
+    // clase insert
+    public void insert(T item, T a[]) {
+        a[++n] = x;
+        T temp;
+        for (j = n; j > 1 && a[j] > a[j / 2]; j /= 2) {
+            // intercambiamos con el padre
+            temp = a[j];
+            a[j] = a[j / 2];
+            a[j / 2] = temp;
+        }
+    }
+
+    // clase eliminar
+    public void eliminar( T a[] ) {
+        m = a[1]; // La variable m lleva el máximo
+        a[1] = a[n--];
+        j = 1;
+        while(2*j < n) { // mientras tenga algún hijo
+            k = 2*j; # el hijo izquierdo
+            if ( k+1 <= n && a[k+1] > a[k])
+                k = k+1; # el hijo derecho es el mayor
+            if ( a[j] > a[k])
+                break; // es mayor que ambos hijos
+        t = a[j];
+        a[j] = a[k];
+        a[k] = t;
+        j = k; // lo intercambiamos con el mayor hijo
+}
+        }
 }
